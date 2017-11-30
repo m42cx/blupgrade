0699c8112ad84daa7b80e401e3537a24d2928c82
xziy - xhozt <cziy@ya.ru>
2017-11-30 04:28:14 +0400

    Feature Drift

--- src/main.h
+++ src/main.h
@@ -55,10 +55,10 @@ static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20
 static const int64_t COIN_YEAR_REWARD = 1 * CENT; // 1% per year
 
 inline bool IsProtocolV1RetargetingFixed(int nHeight) { return TestNet() || nHeight > 38423; }
-inline bool IsProtocolV2(int nHeight) { return TestNet() || nHeight > 4060; }
+inline bool IsProtocolV2(int nHeight) { return TestNet() || nHeight > 319000; }
 inline bool IsProtocolV3(int64_t nTime) { return TestNet() || nTime > 1444028400; }
 
-inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 10 * 60; }
+inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 20 * 60; }
 inline int64_t FutureDriftV2(int64_t nTime) { return nTime + 15; }
 inline int64_t FutureDrift(int64_t nTime, int nHeight) { return IsProtocolV2(nHeight) ? FutureDriftV2(nTime) : FutureDriftV1(nTime); }
 
